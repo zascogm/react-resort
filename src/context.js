6c90c7ddc964330224f68b21c735fc7337887cf5
zascogm <zascogm@gmail.com>
2020-07-04 16:12:53 +1000

    Retreiving local data

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-// import items from './data'
-import Client from './Contentful'
+import items from './data'
+// import Client from './Contentful'
 
 const RoomContext = React.createContext();
 
@@ -22,48 +22,48 @@ class RoomProvider extends Component {
     };
 
     //  getData
-    getData = async () => {
-        try {
-            let response = await Client.getEntries({
-                content_type: "beachResortRoomExample",
-                order: "sys.createdAt"
-            });
-            let rooms = this.formatData(response.items);
-            console.log(rooms);
-            let featuredRooms = rooms.filter(room => room.featured === true);
-            let maxPrice = Math.max(...rooms.map(item => item.price));
-            let maxSize = Math.max(...rooms.map(item => item.size));
+    // getData = async () => {
+    //     try {
+    //         let response = await Client.getEntries({
+    //             content_type: "beachResortRoomExample",
+    //             order: "sys.createdAt"
+    //         });
+    //         let rooms = this.formatData(response.items);
+    //         console.log(rooms);
+    //         let featuredRooms = rooms.filter(room => room.featured === true);
+    //         let maxPrice = Math.max(...rooms.map(item => item.price));
+    //         let maxSize = Math.max(...rooms.map(item => item.size));
 
-            this.setState({
-                rooms, 
-                featuredRooms, 
-                sortedRooms: rooms, 
-                loading: false,
-                price: maxPrice,
-                maxPrice, 
-                maxSize
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    }
+    //         this.setState({
+    //             rooms, 
+    //             featuredRooms, 
+    //             sortedRooms: rooms, 
+    //             loading: false,
+    //             price: maxPrice,
+    //             maxPrice, 
+    //             maxSize
+    //         });
+    //     } catch (error) {
+    //         console.log(error);
+    //     }
+    // }
 
     componentDidMount() {
-        this.getData();
-        // let rooms = this.formatData(items);
-        // let featuredRooms = rooms.filter(room => room.featured === true);
-        // let maxPrice = Math.max(...rooms.map(item => item.price));
-        // let maxSize = Math.max(...rooms.map(item => item.size));
+        // this.getData();
+        let rooms = this.formatData(items);
+        let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
 
-        // this.setState({
-        //     rooms, 
-        //     featuredRooms, 
-        //     sortedRooms: rooms, 
-        //     loading: false,
-        //     price: maxPrice,
-        //     maxPrice, 
-        //     maxSize
-        // });
+        this.setState({
+            rooms, 
+            featuredRooms, 
+            sortedRooms: rooms, 
+            loading: false,
+            price: maxPrice,
+            maxPrice, 
+            maxSize
+        });
     }
 
     formatData(items) {
